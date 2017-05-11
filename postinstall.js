a75eabdbb1b7208dcac3b3d2b4b7dac49e089766
Paul Furiani <pf@furiani.net>
2017-05-11 09:56:16 -0500

    adding reminder to manually move ElixirHelper if desired

--- postinstall.js
+++ postinstall.js
@@ -29,5 +29,6 @@ ncp(source, destination, options, function (err) {
 });
 
 console.log("\nThank you for using Basic Elixir!\n");
+console.log("Don't forget to copy 'ElixirHelper' to your app if you want to use versioning!\n");
 console.log("Please report any issues, bugs or feature requests to:");
 console.log("https://github.com/pfuri/basic-elixir/issues\n");
